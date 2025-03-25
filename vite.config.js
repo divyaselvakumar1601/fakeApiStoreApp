1bf31678b090b762ffabce664e654d745dbbaa21
divyaselvakumar1601 <112465107+divyaselvakumar1601@users.noreply.github.com>
2025-03-26 00:15:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-export default {
-  build: {
-    rollupOptions: {
-      external: [
-        '/src/styles/ProductPages.css'
-      ]
-    }
-  }
-}
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
